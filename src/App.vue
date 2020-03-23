ecd7c66a5bea1bb6e6f85fa1540937ca11a4f08c
Jack Domleo <jackdomleo7@hotmail.com>
2020-03-23 22:58:20 +0000

    Basic Navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <navbar />
     <router-view />
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import Navbar from "@/components/Navbar.vue";
+
+@Component({
+  components: { Navbar },
+})
+export default class App extends Vue {}
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="navbar">
+    
+  </header>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Navbar extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  width: 100%;
+  background-color: grey;
+  height: 3rem;
+}
+</style>
