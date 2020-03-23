ddb820b8f45094ebcb9a9273a722cb94cfdae9dc
Jack Domleo <jackdomleo7@hotmail.com>
2020-03-23 22:01:27 +0000

    Lint

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
-import Router from './router';
+import Router from "./router";
 
 Vue.config.productionTip = false;
 
