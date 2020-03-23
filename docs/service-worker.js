117ec5139d24ecb52a7c11f1b6406d5edee9295c
Jack Domleo <jack.domleo@mitrefinch.co.uk>
2020-03-23 21:56:49 +0000

    Lint

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -11,16 +11,16 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
 importScripts(
-  "/precache-manifest.fcc0822670371f3da0e6b60d97bedc3c.js"
+  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
 );
 
-workbox.core.setCacheNameDetails({prefix: "tmdip"});
+importScripts("/precache-manifest.fcc0822670371f3da0e6b60d97bedc3c.js");
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
+workbox.core.setCacheNameDetails({ prefix: "tmdip" });
+
+self.addEventListener("message", event => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
 });
