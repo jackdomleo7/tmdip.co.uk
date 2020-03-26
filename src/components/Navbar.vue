719ec608fbfc24acd4b9a0490de5554345400d4a
Jack Domleo <jackdomleo7@hotmail.com>
2020-03-26 00:56:54 +0000

    Better styled navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,22 +1,123 @@
 <template>
   <header class="navbar">
-    
+    <ul class="navbar__list">
+      <li v-for="(navItem, index) in navList" :key="index" class="navbar__navitem">
+        <router-link v-if="navItem.link" :to="navItem.link" class="navbar__link">
+          {{ navItem.text }}
+        </router-link>
+        <div v-else-if="navItem.subList" class="navbar__dropdown">
+          <span class="navbar__link">{{ navItem.text }}</span>
+          <ul class="navbar__sublist">
+            <li v-for="(subItem, index) in navItem.subList" :key="index" class="navbar__subitem">
+              <router-link :to="subItem.link">
+                {{ subItem.text }}
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </li>
+    </ul>
   </header>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 
+interface INavList {
+  text: string,
+  link?: string | object,
+  subList?: INavSubList[]
+}
+
+interface INavSubList {
+  text: string,
+  link: string | object
+}
+
 @Component
-export default class Navbar extends Vue {}
+export default class Navbar extends Vue {
+  private readonly navList: INavList[] = [
+    {
+      text: 'Home',
+      link: '/',
+    },
+    {
+      text: 'Interior fit outs',
+      link: '/interior-fit-outs'
+    },
+    {
+      text: 'Timber homes',
+      link: '/timber-homes'
+    },
+    {
+      text: 'Policies',
+      subList: [
+        {
+          text: 'Modern Slavery Act',
+          link: '#'
+        },
+        {
+          text: 'Health & Safety',
+          link: '#'
+        },
+        {
+          text: 'Environmental Policy',
+          link: '#'
+        },
+        {
+          text: 'Quality Managemnet System',
+          link: '#'
+        },
+        {
+          text: 'Privacy Policy',
+          link: '#'
+        },
+        {
+          text: 'ICO',
+          link: '#'
+        },
+      ]
+    }
+  ];
+}
 </script>
 
 <style lang="scss" scoped>
 .navbar {
   width: 100%;
   background-color: var(--color-grey-400);
-  height: 5rem;
   position: sticky;
-  top: 0;;
+  top: 0;
+  display: inline-block;
+  font-size: 1rem;
+
+  &__list {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    height: 100%;
+  }
+
+  &__navitem {
+    position: relative;
+    float: left;
+
+    &:hover {
+      .navbar__sublist {
+        display: block;
+      }
+    }
+  }
+
+  &__link {
+    display: block;
+    padding: 1.2rem;
+    cursor: pointer;
+  }
+
+  &__sublist {
+    position: absolute;
+    display: none;
+  }
 }
 </style>
