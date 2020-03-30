0b89012786f75db928e3f4bdf38db0aa7e77ed4f
Jack Domleo <jackdomleo7@hotmail.com>
2020-03-30 16:37:45 +0100

    Pulled navigation into separate directory

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,7 @@
 <template>
   <div id="app">
     <jumbo />
-    <button
-      class="navbar__toggler"
-      :class="isMobileNavDisplayed ? 'navbar__toggler--fixed' : null"
-      @click="isMobileNavDisplayed = !isMobileNavDisplayed"
-      :title="(isMobileNavDisplayed ? 'Hide' : 'Show') + ' navigation'"
-    >
-      <span hidden>{{
-        (isMobileNavDisplayed ? "Hide" : "Show") + " navigation"
-      }}</span>
-      <svg class="navbar__toggler-icon">
-        <use
-          :xlink:href="
-            'assets/svg-sprite.svg#icon-' +
-              (isMobileNavDisplayed ? 'close' : 'hamburger')
-          "
-        ></use>
-      </svg>
-    </button>
-    <navbar :show-mobile-nav="isMobileNavDisplayed" />
+    <navigation />
     <router-view style="flex: 1;" />
     <footer-bar />
   </div>
@@ -29,15 +11,13 @@
 import "normalize.css";
 import { Component, Vue } from "vue-property-decorator";
 import Jumbo from "@/components/jumbo/Jumbo.vue";
-import Navbar from "@/components/Navbar.vue";
+import Navigation from "@/components/navigation/Navigation.vue";
 import FooterBar from "@/components/FooterBar.vue";
 
 @Component({
-  components: { Jumbo, Navbar, FooterBar }
+  components: { Jumbo, Navigation, FooterBar }
 })
 export default class App extends Vue {
-  private isMobileNavDisplayed = false;
-
   private get companyName() {
     return process.env.VUE_APP_NAME;
   }
@@ -46,31 +26,6 @@ export default class App extends Vue {
 
 <style lang="scss">
 @import "./scss/main";
-
-.navbar__toggler {
-  background-color: transparent;
-  border: none;
-  cursor: pointer;
-  padding: 1rem;
-  position: absolute;
-  right: 1rem;
-  top: 1rem;
-  z-index: 999;
-
-  @media (min-width: 45.625em) {
-    display: none !important;
-  }
-
-  &--fixed {
-    position: fixed;
-  }
-
-  &-icon {
-    color: var(--color-grey-50);
-    height: 2rem;
-    width: 2rem;
-  }
-}
 
 #app {
   display: flex;
