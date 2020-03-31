2ad056b68920a748b63837abe6b9a3f60f691fc1
Jack Domleo <jack.domleo@mitrefinch.co.uk>
2020-03-31 23:34:28 +0100

    Quick action bar template

---
+++ src/layout/QuickActionBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="qab">
+    
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { Icon, SiteLink } from "@/components";
+import CompanyInfo, { TelNumber } from "@/helpers/companyInfo";
+
+@Component({
+  components: { Icon, SiteLink }
+})
+export default class QuickActionBar extends Vue {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+  .qab {
+    width: 100vw;
+    height: 4rem;
+    background-color: var(--color-grey-75);
+    position: sticky;
+    bottom: 0;
+  }
+</style>
