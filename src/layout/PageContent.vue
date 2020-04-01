a3fecb429fd84be486f502eebd85e5a36234afe2
Jack Domleo <jackdomleo7@hotmail.com>
2020-04-01 13:22:43 +0100

    Initial PageContent component

---
+++ src/layout/PageContent.vue
@@ -0,0 +1,18 @@
+<template>
+  <main class="content">
+    <router-view />
+  </main>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class PageContent extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.content {
+  flex: 1;
+}
+</style>
