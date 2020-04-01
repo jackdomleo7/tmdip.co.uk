a56cf20fcfdc824305a54898cd5a2ea758a8d6e7
Jack Domleo <jackdomleo7@hotmail.com>
2020-04-01 10:17:58 +0100

    Lint

--- src/layout/QuickActionBar.vue
+++ src/layout/QuickActionBar.vue
@@ -1,10 +1,20 @@
 <template>
   <section class="qab">
-    <site-link class="qab__link" type="email" :link="companyEmail" :title="'Email us today at ' + companyEmail">
+    <site-link
+      class="qab__link"
+      type="email"
+      :link="companyEmail"
+      :title="'Email us today at ' + companyEmail"
+    >
       <icon class="qab__icon qab__icon--email" icon="email" />
       <span hidden>Email us today at {{ companyEmail }}</span>
     </site-link>
-    <site-link class="qab__link" type="tel" :link="companyTelNumber.tel" :title="'Call us today ' + companyTelNumber.telPretty">
+    <site-link
+      class="qab__link"
+      type="tel"
+      :link="companyTelNumber.tel"
+      :title="'Call us today ' + companyTelNumber.telPretty"
+    >
       <icon class="qab__icon qab__icon--tel" icon="phone" />
       <span hidden>Call us today on {{ companyTelNumber.telPretty }}</span>
     </site-link>
@@ -31,38 +41,38 @@ export default class QuickActionBar extends Vue {
 </script>
 
 <style lang="scss" scoped>
-  .qab {
-    width: 100vw;
-    height: 3rem;
-    background-color: var(--color-grey-75);
-    position: sticky;
-    bottom: 0;
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    overflow: hidden;
-    box-shadow: var(--color-grey-500) 0 -0.25rem 0.5rem 0;
+.qab {
+  align-items: center;
+  background-color: var(--color-grey-75);
+  bottom: 0;
+  box-shadow: var(--color-grey-500) 0 -0.25rem 0.5rem 0;
+  display: flex;
+  height: 3rem;
+  justify-content: space-around;
+  overflow: hidden;
+  position: sticky;
+  width: 100vw;
 
-    &__link {
-      height: 100%;
-      width: 50%;
-      text-align: center;
+  &__link {
+    height: 100%;
+    text-align: center;
+    width: 50%;
+  }
+
+  &__icon {
+    width: 2rem;
+
+    &--email {
+      color: var(--color-red);
     }
 
-    &__icon {
-      width: 2rem;
-
-      &--email {
-        color: var(--color-red);
-      }
-
-      &--tel {
-        color: var(--color-green);
-      }
-    }
-
-    @media (min-width: 45.625em) {
-      display: none;
+    &--tel {
+      color: var(--color-green);
     }
   }
+
+  @media (min-width: 45.625em) {
+    display: none;
+  }
+}
 </style>
