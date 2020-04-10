e6028e0c5147f0d26422cd09e40e124416d81c7b
Jack Domleo <jackdomleo7@hotmail.com>
2020-04-10 21:58:10 +0100

    Initial home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,32 @@
 <template>
-  <div class="home">
-    Home
-  </div>
+  <page-content>
+    
+  </page-content>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { Routes } from "@/router";
+import { PageContent } from "@/layout";
+import { SiteLink } from "@/components";
+import CompanyInfo from "@/helpers/companyInfo";
+
+@Component({
+  components: { PageContent, SiteLink }
+})
+export default class PageNotFound extends Vue {
+  private Routes: Routes = Routes;
+
+  private get path(): string {
+    return this.$route.path;
+  }
+
+  private get companyName(): string {
+    return CompanyInfo.companyName;
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
