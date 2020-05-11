9d8415ec47586c0b9c443586cc1539c10e61a788
Jack Domleo <jackdomleo7@hotmail.com>
2020-05-11 22:24:26 +0100

    Copied over home page and images from Vue app

--- components/index.ts
+++ components/index.ts
@@ -3,11 +3,17 @@ import SiteLink from './SiteLink.vue';
 import Navigation from './nav/Navigation.vue';
 import FooterBar from './FooterBar.vue';
 import QuickActionBar from './QuickActionBar.vue';
+import PageTemplate from './PageTemplate.vue';
+// import TheMap from './TheMap.vue';
+import Carousel from './Carousel.vue';
 
 export {
   Hero,
   SiteLink,
   Navigation,
   FooterBar,
-  QuickActionBar
+  QuickActionBar,
+  PageTemplate,
+  // TheMap,
+  Carousel
 }
