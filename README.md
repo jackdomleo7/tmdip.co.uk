2468221ca6b75fcf510be771401e4f3bca1cb96b
Jack Domleo <jackdomleo7@hotmail.com>
2020-05-11 12:49:20 +0100

    Added favicons. head meta, document title and open graph

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-# tmdip.co.uk
+# [tmdip.co.uk](https://www.tmdip.co.uk)
 
-> Website for TMD Interior Projects &amp; Building Services Ltd
+> Website for TMD Interior Projects & Building Services Ltd
+
+https://www.tmdip.co.uk
+
+![Website status](https://img.shields.io/website?down_color=red&down_message=offline&up_color=green&up_message=online&url=https%3A%2F%2Fwww.tmdip.co.uk "Website status")
 
 ## Build Setup
 
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,56 +1,57 @@
+const companyName = 'TMD Interior Projects & Building Services';
 
 export default {
   mode: 'universal',
-  /*
-  ** Headers of the page
-  */
   head: {
-    title: process.env.npm_package_name || '',
+    titleTemplate: `%s | Shop fitting, Interior fit-outs, Timber homes | ${companyName}`,
     meta: [
       { charset: 'utf-8' },
+      { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
+      { name: 'robots', content: 'index, follow' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { name: 'msapplication-TileColor', content: '#2b5797' },
+      { name: 'msapplication-TileImage', content: '/favicons/mstile-144x144.png' },
+      { name: 'theme-color', content: '#ffffff' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicons/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicons/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicons/favicon-16x16.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/favicons/safari-pinned-tab.svg', color: '#f0592a' }
     ]
   },
-  /*
-  ** Customize the progress-bar color
-  */
+  pwa: {
+    meta: {
+      name: companyName,
+      author: `Jack Domleo, ${companyName}`,
+      description:
+        'Experts in retail, interior fit-outs and timber homes. From building new bespoke houses to HMOs, from shops to offices and pubs to restaurants.',
+      ogType: 'website',
+      ogHost: 'https://www.tmdip.co.uk',
+      ogImage: {
+        path: '/open-graph.png',
+        width: '1200',
+        height: '630',
+        type: 'image/png'
+      },
+      twitterCard: 'summary_large_image',
+      twitterSite: '@jackdomleo7',
+      twitterCreator: '@jackdomleo7'
+    }
+  },
   loading: { color: '#fff' },
-  /*
-  ** Global CSS
-  */
   css: [
   ],
-  /*
-  ** Plugins to load before mounting the App
-  */
   plugins: [
   ],
-  /*
-  ** Nuxt.js dev-modules
-  */
   buildModules: [
     '@nuxt/typescript-build',
   ],
-  /*
-  ** Nuxt.js modules
-  */
   modules: [
     '@nuxtjs/pwa',
     // Doc: https://github.com/nuxt-community/dotenv-module
     '@nuxtjs/dotenv',
-  ],
-  /*
-  ** Build configuration
-  */
-  build: {
-    /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
-  }
+  ]
 }
