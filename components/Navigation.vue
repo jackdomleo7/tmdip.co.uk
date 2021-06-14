8bf753d747daae526e5ac1a17d555ddc3829bde9
Jack Domleo <jackdomleo7@gmail.com>
2021-06-14 09:29:52 +0100

    Basic mobile navigation

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,12 +1,26 @@
 <template>
   <nav class="nav">
-    <ul class="nav__primary">
-      <li v-for="(navItem, index) in navItems" :key="navItem.text" :aria-setsize="navItems.length" :aria-posinset="index + 1">
-        <nuxt-link v-if="navItem.url" :to="navItem.url">
+    <div class="nav__mobile">
+      <a :href="`mailto:${siteconfig.email}`" target="_blank" rel="noopener noreferrer">
+        <svg-icon name="mail" />
+        Email Us
+      </a>
+      <button class="nav__hamburger" @click="showMobileNav = !showMobileNav">
+        <span></span>
+        <svg-icon name="menu" />
+      </button>
+      <a :href="`tel:${siteconfig.tel.country.replace(/\s/g, '')}`" target="_blank" rel="noopener noreferrer">
+        <svg-icon name="phone" />
+        Call Us
+      </a>
+    </div>
+    <ul class="nav__primary" :class="{'nav__primary--mobile-open': isMobile && showMobileNav}">
+      <li v-for="(navItem, index) in navItems" :key="navItem.text" :aria-setsize="navItems.length" :aria-posinset="index + 1" :class="{ 'open': displayedMobileNavItemIndex === index }">
+        <nuxt-link v-if="navItem.url" :to="navItem.url" @click="navItemClick($event, index)">
           {{ navItem.text }}
           <svg-icon v-if="navItem.subMenu" name="chevron-down" />
         </nuxt-link>
-        <a v-else href="javascript:void(0)">
+        <a v-else href="javascript:void(0)" @click="navItemClick($event, index)">
           {{ navItem.text }}
           <svg-icon v-if="navItem.subMenu" name="chevron-down" />
         </a>
@@ -38,6 +52,9 @@ export default Vue.extend({
   data () {
     return {
       siteconfig,
+      isMobile: false,
+      showMobileNav: false,
+      displayedMobileNavItemIndex: null as null | number,
       navItems: [
         {
           text: 'Home',
@@ -100,98 +117,286 @@ export default Vue.extend({
       ] as INav[]
     }
   },
-  props: {
-    header: { // To be replaced with asyncData in Nuxt 3
-      type: Object,
-      required: true
+  watch: {
+    // Close navigation menu on page change
+    '$route' () {
+      this.showMobileNav = false
+      this.displayedMobileNavItemIndex = null
+    }
+  },
+  mounted (): void {
+    this.setResponsiveness()
+    window.addEventListener('resize', this.setResponsiveness)
+  },
+  destroyed () {
+    window.removeEventListener('resize', this.setResponsiveness)
+  },
+  methods: {
+    setResponsiveness (): void {
+      const navBreak = 54.5 // Number should match em value on $nav-break in below SCSS
+      this.isMobile = window.matchMedia(`max-width: ${navBreak}em`).matches || window.innerWidth <= (navBreak * 16 /* em to px */)
+    },
+    navItemClick(event: Event, index: number): void {
+      if (this.isMobile && this.navItems[index].subMenu) {
+        console.log(index)
+        console.log(this.displayedMobileNavItemIndex)
+        event.preventDefault()
+        this.displayedMobileNavItemIndex = this.displayedMobileNavItemIndex === index ? null : index
+      }
     }
   }
 })
 </script>
 
 <style lang="scss" scoped>
+$nav-break: 54.5em;
+$nav-height: 3.8rem;
+
 .nav {
-  margin-bottom: 1rem;
-  border-bottom: 1px solid var(--color-black);
-  position: relative;
-
-  &::before {
-    content: '';
-    display: block;
-    border-top: 1px solid var(--color-black);
-    width: 80%;
-    position: absolute;
-    top: 0;
-    left: 50%;
-    transform: translateX(-50%);
-  }
-
-  &__primary {
-    display: flex;
-    align-items: center;
-    justify-content: space-evenly;
-    list-style-type: none;
-    padding-left: 0;
-    margin: 0;
-
-    li {
-      position: relative;
-
-      @media (hover: hover) {
-        &:hover a,
-        a:focus,
-        &:focus-within a {
-          + .nav__submenu {
-            opacity: 1;
-            visibility: visible;
-          }
-        }
-      }
-    }
-    
-    a {
-      font-weight: 700;
-      color: var(--color-black);
-      text-decoration: none;
-      padding: 1rem;
+  @media (max-width: $nav-break - (1em/16)) {
+    &__mobile {
+      position: fixed;
+      z-index: 2;
+      bottom: 0;
+      left: 0;
+      width: 100vw;
       display: flex;
       align-items: center;
+      justify-content: space-around;
+      background-color: var(--color-grey-lightest);
+      height: $nav-height;
+      box-shadow: 0 0 8px 2px rgba(0,0,0,0.2);
 
-      &[href="javascript:void(0)"] {
-        cursor: default;
+      a {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        font-size: 0.75rem;
+        text-decoration: none;
+        color: var(--color-black);
+
+        svg {
+          height: 1.8rem;
+          width: 1.8rem;
+          fill: var(--color-black);
+        }
       }
 
-      @media (hover: hover) {
-        &:hover {
-          background-color: var(--color-orange-o50);
+      button {
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: relative;
+        bottom: 25%;
+        width: 3.375rem;
+        height: 1.9375rem;
+        background-color: var(--color-orange);
+        border: none;
+        margin: 1rem 0;
+        box-shadow: 0 0 8px rgba(0,0,0,0.4);
 
-          svg {
-            color: var(--color-black);
-          }
+        &::before,
+        &::after {
+          content: "";
+          position: absolute;
+          z-index: 1;
+          width: 2.375rem;
+          height: 2.375rem;
+          transform: scaleY(0.5774) rotate(-45deg);
+          background-color: inherit;
+          left: 0.5rem;
+          box-shadow: 0 0 8px rgba(0,0,0,0.4);
+        }
+
+        &::before {
+          top: -1.1875rem;
+        }
+
+        &::after {
+          bottom: -1.1875rem;
+        }
+
+        span {
+          display: block;
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 3.375rem;
+          height: 1.9375rem;
+          z-index: 2;
+          background: inherit;
+        }
+
+        svg {
+          height: 2.4rem;
+          width: 2.4rem;
+          fill: var(--color-white);
+          position: relative;
+          z-index: 2;
         }
       }
     }
 
-    svg {
-      width: 1.2rem;
-      height: 1.2rem;
-      color: var(--color-orange);
+    &__primary {
+      position: fixed;
+      z-index: 1;
+      top: 100%;
+      transition: top 0.4s ease;
+      will-change: top;
+      background-color: var(--color-white);
+      box-shadow: 0 0 8px 2px rgba(0,0,0,0.2);
+      height: calc(100vh - #{$nav-height});
+      overflow-y: auto;
+      padding-left: 0;
+      margin: 0;
+      padding: 2rem 0;
+      list-style-type: none;
+      width: 100vw;
+
+      &--mobile-open {
+        top: 0;
+      }
+
+      li {
+        max-width: 100%;
+        width: 100%;
+        border-bottom: 1px solid transparent;
+        overflow: hidden;
+        max-height: 3rem;
+        transition: max-height 160ms ease;
+        will-change: max-height;
+
+        &:not(:last-of-type) {
+          border-bottom-color: var(--color-grey-light);
+        }
+
+        &.open {
+          max-height: 9999rem;
+
+          ul {
+            transform: translateY(0);
+          }
+        }
+      }
+
+      a {
+        text-decoration: none;
+        font-weight: 700;
+        color: var(--color-black);
+        background-color: var(--color-white);
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 0.8rem 1rem;
+        position: relative;
+        z-index: 12;
+
+        svg {
+          height: 1.6rem;
+          width: 1.6rem;
+        }
+      }
+
+      ul {
+        transform: translateY(-115%);
+        transition: transform 160ms ease;
+        will-change: transform;
+        position: relative;
+        z-index: 11;
+      }
     }
   }
 
-  &__submenu {
-    position: absolute;
-    top: 100%;
-    opacity: 0;
-    visibility: hidden;
-    transition: all 160ms ease;
-    will-change: opacity, visibility;
-    border: 1px solid var(--color-black);
-    border-top-color: transparent;
-    background-color: var(--color-white);
-    padding-left: 0;
-    list-style-type: none;
-    width: 12.5rem;
+  @media (min-width: $nav-break) {
+    margin-bottom: 1rem;
+    border-bottom: 1px solid var(--color-black);
+    position: relative;
+
+    &__mobile {
+      display: none;
+    }
+
+    &::before {
+      content: '';
+      display: block;
+      border-top: 1px solid var(--color-black);
+      width: 80%;
+      position: absolute;
+      top: 0;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+
+    &__primary {
+      position: unset;
+      display: flex;
+      align-items: center;
+      justify-content: space-evenly;
+      list-style-type: none;
+      padding-left: 0;
+      margin: 0;
+
+      li {
+        position: relative;
+
+        @media (hover: hover) {
+          &:hover a,
+          a:focus,
+          &:focus-within a {
+            + .nav__submenu {
+              opacity: 1;
+              visibility: visible;
+            }
+          }
+        }
+      }
+      
+      a {
+        font-weight: 700;
+        color: var(--color-black);
+        text-decoration: none;
+        padding: 1rem;
+        display: flex;
+        align-items: center;
+
+        &[href="javascript:void(0)"] {
+          cursor: default;
+        }
+
+        @media (hover: hover) {
+          &:hover {
+            background-color: var(--color-orange-o50);
+
+            svg {
+              color: var(--color-black);
+            }
+          }
+        }
+      }
+
+      svg {
+        width: 1.2rem;
+        height: 1.2rem;
+        color: var(--color-orange);
+      }
+    }
+
+    &__submenu {
+      position: absolute;
+      top: 100%;
+      opacity: 0;
+      visibility: hidden;
+      transition: 160ms ease;
+      transition-property: opacity, visibility;
+      will-change: opacity, visibility;
+      border: 1px solid var(--color-black);
+      border-top-color: transparent;
+      background-color: var(--color-white);
+      padding-left: 0;
+      list-style-type: none;
+      width: 12.5rem;
+    }
   }
 }
 </style>
